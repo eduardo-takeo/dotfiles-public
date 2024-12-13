036948629be249348941f0868432964e5982a64c
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2024-12-13 19:42:14 -0300

    install lazyvim distro

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,9 @@
 set fish_greeting ""
 
+fish_add_path /opt/homebrew/bin/
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
+# source /opt/homebrew/opt/asdf/libexec/asdf.fish
