a4e9cec49791fa543602ff55a7d5ed28b3c18192
Eduardo <eduardo.higashi@ab-inbev.com>
2025-01-16 19:20:45 -0300

    add devaslife nvim config

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 10000,
+    },
+  },
+  {
+    "snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+    },
+    keys = {},
+  },
+}
